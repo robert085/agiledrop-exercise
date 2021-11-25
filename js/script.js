7089ab3230917ea16da4a9008037a59c5d24cee8
robert085 <rkunc9398@gmail.com>
2021-11-25 21:33:50 +0100

    updating packages

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// OFF-CANVAS
+document.querySelector('.button-offc').addEventListener('click', function () {
+  document.querySelector('.off-canvas').classList.remove('hidden');
+});
+
